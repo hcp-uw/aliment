1c0edd1bd2353685aefa6710af56d83c92a30c41
Ethan Tran <ethantran@Ethans-MacBook-Pro-7.local>
2024-02-05 15:58:46 -0800

    changes reverted

--- starter-backend/server.js
+++ starter-backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 // test endpoint
 app.get('/hello', (req, res) => { 
-    res.send('Attention HCP Project Team! If you see this, your front end and back end are connected, + editing works.') 
+    res.send('Attention HCP Project Team! If you see this, your front end and back end are connected.') 
 })
 
 // error handling
