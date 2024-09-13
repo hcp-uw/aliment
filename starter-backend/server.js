c3a778df1d7bd57a14e6c3021279ee6cfff0c6bc
Emily <emilyngo@cs.washington.edu>
2024-09-12 21:37:40 -0700

    changed  middleware, package-lock, package, and server

--- starter-backend/server.js
+++ starter-backend/server.js
@@ -1,6 +1,10 @@
-const express = require("express")
-const cors = require("cors")
-const { unknownEndpoint } = require('./middleware');
+// const express = require("express")
+// const cors = require("cors")
+// const { unknownEndpoint } = require('./middleware');
+import express from "express";
+import cors from "cors";
+import { fetchAllRecipes, fetchFilteredRecipes, fetchRecipesByIngredients } from './matchRecipes.js';
+import { unknownEndpoint } from './middleware.js';
 
 // create your express application
 const app = express();
@@ -8,10 +12,39 @@ const app = express();
 // enable cors
 app.use(cors());
 
+// [added] allowing the server to parse JSON requests
+app.use(express.json());
+
 // test endpoint
 app.get('/hello', (req, res) => { 
-    res.send('Attention HCP Project Team! If you see this, your front end and back end are connected.') 
-})
+  res.send('Attention HCP Project Team! If you see this, your front end and back end are connected.') 
+});
+
+// [added] API route to get all the recipes
+app.get('/recipes', async (req, res) => {
+  const recipes = await fetchAllRecipes();
+  res.json(recipes);
+});
+
+// [added] API to filter the recipes
+app.post('/filter', async (req, res) => {
+  const { categories } = req.body;
+  const filteredRecipes = await fetchFilteredRecipes(categories);
+  res.json(filteredRecipes);
+});
+
+// [added] API to search recipes by ingredients
+app.post('/search', async (req, res) => {
+  const { searchTerm } = req.body;
+
+  try {
+    const recipes = await fetchRecipesByIngredients(searchTerm);
+    res.json(recipes);
+  } catch (error) {
+    console.error('Error searching recipes:', error);
+    res.status(500).json([]);
+  }
+});
 
 // error handling
 app.use(unknownEndpoint);
@@ -21,5 +54,5 @@ const PORT = 3001;
 
 // start your server
 app.listen(PORT, () => {
-    console.log(`Server running on port test ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
