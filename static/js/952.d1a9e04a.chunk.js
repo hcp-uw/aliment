44512727b63b76c53e6379ba70bae8fc57c67d70
Emily Ngo <emilyngo80@gmail.com>
2024-10-14 19:25:07 -0700

    Updates

---
+++ static/js/952.d1a9e04a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_recipe_app=self.webpackChunkmy_recipe_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.d1a9e04a.chunk.js.map
